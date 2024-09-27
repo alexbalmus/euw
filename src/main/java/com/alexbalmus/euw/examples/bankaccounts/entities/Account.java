faeb7e24b705fa406b2123740d177dbef13a111f
alexbalmus <alexbalmus@gmail.com>
2024-09-27 10:47:41 +0300

    Minor change

--- src/main/java/com/alexbalmus/euw/examples/bankaccounts/entities/Account.java
+++ src/main/java/com/alexbalmus/euw/examples/bankaccounts/entities/Account.java
@@ -6,11 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+
 @Entity
 @Table(name="account")
+@Getter
+@Setter
 public class Account
 {
     @Id
